9e0f9c4f15841d027815c4e655da59ee2fc80d02
Zeynep Ceyhan <104215922+zeynepnceyhan@users.noreply.github.com>
2022-08-01 13:23:47 +0300

    Methods
    internship

---
+++ Methods1/Methods1/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Add();
+            Console.ReadLine();
+        }
+        static void Add()
+        {
+            Console.WriteLine("Added!");
+        }
+
+    }
+}
